8786116ac59437d63b3b4edf4bbc6e4896ff6da6
willfryatt <willfryatt@gmail.com>
2025-07-14 15:14:57 +0100

    union_month_query.sql

--- union_month_query.sql
+++ union_month_query.sql
@@ -2,192 +2,192 @@
 
 CREATE TABLE "tripdata_202405_202503" AS
 SELECT
-ride_id,
-started_at,
-ended_at,
-member_casual,
-hour,
-ride_length,
-day_of_week,
-month,
-year,
-day_of_week_no,
-month_no
+  ride_id,
+  started_at,
+  ended_at,
+  member_casual,
+  hour,
+  ride_length,
+  day_of_week,
+  month,
+  year,
+  day_of_week_no,
+  month_no
 FROM
 tripdata_202404
 UNION
 
 SELECT
-ride_id,
-started_at,
-ended_at,
-member_casual,
-hour,
-ride_length,
-day_of_week,
-month,
-year,
-day_of_week_no,
-month_no
+  ride_id,
+  started_at,
+  ended_at,
+  member_casual,
+  hour,
+  ride_length,
+  day_of_week,
+  month,
+  year,
+  day_of_week_no,
+  month_no
 FROM
 tripdata_202405
 UNION
 
 SELECT
-ride_id,
-started_at,
-ended_at,
-member_casual,
-hour,
-ride_length,
-day_of_week,
-month,
-year,
-day_of_week_no,
-month_no
+  ride_id,
+  started_at,
+  ended_at,
+  member_casual,
+  hour,
+  ride_length,
+  day_of_week,
+  month,
+  year,
+  day_of_week_no,
+  month_no
 FROM
 tripdata_202406
 UNION
 
 SELECT
-ride_id,
-started_at,
-ended_at,
-member_casual,
-hour,
-ride_length,
-day_of_week,
-month,
-year,
-day_of_week_no,
-month_no
+  ride_id,
+  started_at,
+  ended_at,
+  member_casual,
+  hour,
+  ride_length,
+  day_of_week,
+  month,
+  year,
+  day_of_week_no,
+  month_no
 FROM
 tripdata_202407
 UNION
 
 SELECT
-ride_id,
-started_at,
-ended_at,
-member_casual,
-hour,
-ride_length,
-day_of_week,
-month,
-year,
-day_of_week_no,
-month_no
+  ride_id,
+  started_at,
+  ended_at,
+  member_casual,
+  hour,
+  ride_length,
+  day_of_week,
+  month,
+  year,
+  day_of_week_no,
+  month_no
 FROM
 tripdata_202408
 UNION
 
 SELECT
-ride_id,
-started_at,
-ended_at,
-member_casual,
-hour,
-ride_length,
-day_of_week,
-month,
-year,
-day_of_week_no,
-month_no
+  ride_id,
+  started_at,
+  ended_at,
+  member_casual,
+  hour,
+  ride_length,
+  day_of_week,
+  month,
+  year,
+  day_of_week_no,
+  month_no
 FROM
 tripdata_202409
 UNION
 
 SELECT
-ride_id,
-started_at,
-ended_at,
-member_casual,
-hour,
-ride_length,
-day_of_week,
-month,
-year,
-day_of_week_no,
-month_no
+  ride_id,
+  started_at,
+  ended_at,
+  member_casual,
+  hour,
+  ride_length,
+  day_of_week,
+  month,
+  year,
+  day_of_week_no,
+  month_no
 FROM
 tripdata_202410
 UNION
 
 SELECT
-ride_id,
-started_at,
-ended_at,
-member_casual,
-hour,
-ride_length,
-day_of_week,
-month,
-year,
-day_of_week_no,
-month_no
+  ride_id,
+  started_at,
+  ended_at,
+  member_casual,
+  hour,
+  ride_length,
+  day_of_week,
+  month,
+  year,
+  day_of_week_no,
+  month_no
 FROM
 tripdata_202411
 UNION
 
 SELECT
-ride_id,
-started_at,
-ended_at,
-member_casual,
-hour,
-ride_length,
-day_of_week,
-month,
-year,
-day_of_week_no,
-month_no
+  ride_id,
+  started_at,
+  ended_at,
+  member_casual,
+  hour,
+  ride_length,
+  day_of_week,
+  month,
+  year,
+  day_of_week_no,
+  month_no
 FROM
 tripdata_202412
 UNION
 
 SELECT
-ride_id,
-started_at,
-ended_at,
-member_casual,
-hour,
-ride_length,
-day_of_week,
-month,
-year,
-day_of_week_no,
-month_no
+  ride_id,
+  started_at,
+  ended_at,
+  member_casual,
+  hour,
+  ride_length,
+  day_of_week,
+  month,
+  year,
+  day_of_week_no,
+  month_no
 FROM
 tripdata_202501
 UNION
 
 SELECT
-ride_id,
-started_at,
-ended_at,
-member_casual,
-hour,
-ride_length,
-day_of_week,
-month,
-year,
-day_of_week_no,
-month_no
+  ride_id,
+  started_at,
+  ended_at,
+  member_casual,
+  hour,
+  ride_length,
+  day_of_week,
+  month,
+  year,
+  day_of_week_no,
+  month_no
 FROM
 tripdata_202502
 UNION
 
 SELECT
-ride_id,
-started_at,
-ended_at,
-member_casual,
-hour,
-ride_length,
-day_of_week,
-month,
-year,
-day_of_week_no,
-month_no
+  ride_id,
+  started_at,
+  ended_at,
+  member_casual,
+  hour,
+  ride_length,
+  day_of_week,
+  month,
+  year,
+  day_of_week_no,
+  month_no
 FROM
 tripdata_202503;
